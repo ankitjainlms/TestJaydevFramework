465f55144733890fef66fb82f86aca707a1112a9
mudit jain <mudit.jain@lmsin.com>
2018-07-27 20:22:11 +0530

    message

---
+++ TestJaydevFramework/TestJaydevFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  TestJaydevFramework
+//
+//  Created by LMS on 27/07/18.
+//  Copyright © 2018 LMS. All rights reserved.
+//
+
+import Foundation
+
+class Servic{
+    
+    public static func myMethod()->String
+    {
+        return "jaydev"
+    }
+    
+}
---
+++ TestJaydevFramework/TestJaydevFramework/TestJaydevFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestJaydevFramework.h
+//  TestJaydevFramework
+//
+//  Created by LMS on 27/07/18.
+//  Copyright © 2018 LMS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestJaydevFramework.
+FOUNDATION_EXPORT double TestJaydevFrameworkVersionNumber;
+
+//! Project version string for TestJaydevFramework.
+FOUNDATION_EXPORT const unsigned char TestJaydevFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestJaydevFramework/PublicHeader.h>
+
+
